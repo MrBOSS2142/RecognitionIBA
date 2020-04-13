099405fb4606d16320c8f8d7319651a524cd5f25
Sergey Sukharevich <mr.boss2142bf@gmail.com>
2020-04-13 16:20:21 +0300

    Create recognition page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="h-100">
+    <b-navbar toggleable="lg" type="dark" variant="dark" class="top-position" >
+      <b-navbar-brand>RecognitionIBA</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse">
+      </b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown right>
+            <template v-slot:button-content>
+              <b>USER NAME</b>
+            </template>
+            <b-dropdown-item href="#">Профиль</b-dropdown-item>
+            <b-dropdown-item href="#">Выход</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+
+    <Recognition />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Recognition from "./components/Recognition";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Recognition
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  html {
+    height: 100%;
+  }
+  body{
+    height: 100%;
+    width: 100%;
+    padding: 0;
+    margin: 0;
+  }
+  .top-position{
+    z-index: 10000;
+  }
+  .padd {
+    padding: 30px;
+  }
 </style>
---
+++ src/components/Recognition.vue
@@ -0,0 +1,68 @@
+<template>
+    <b-container class="bv-example-row mt-3" fluid>
+        <b-row>
+            <b-col cols="8">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/4OjiOn5s8s8"
+                            allowfullscreen></iframe>
+                    <!--                    <video autoplay="true" id="videoElement"></video>-->
+                </div>
+                <div class="embed-responsive embed-responsive-16by9 mt-3">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Q0oIoR9mLwc"
+                            allowfullscreen></iframe>
+                </div>
+            </b-col>
+            <b-col cols="4">
+                <div class="cards fill">
+                    <div class="container padd">
+                        <div class="idcard">
+                            <div class="container" fluid>
+                                <b-row>
+                                    <b-col cols="5">
+                                        <div class="photo">asdasdas</div>
+                                    </b-col>
+
+                                    <b-col cols="7">
+                                        <div class="info">asdasd</div>
+                                    </b-col>
+                                </b-row>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+    export default {
+        name: "Recognition"
+    };
+
+    // var video = document.querySelector("#videoElement");
+    //
+    // if (navigator.mediaDevices.getUserMedia) {
+    //     navigator.mediaDevices.getUserMedia({ video: true })
+    //         .then(function (stream) {
+    //             video.srcObject = stream;
+    //         })
+    // }
+
+</script>
+
+<style scoped>
+    .cards {
+        background: darkgray;
+        height: 100%;
+    }
+
+    .idcard {
+        background: #2c3e50;
+        height: 200px;
+    }
+
+    .padd {
+        padding: 30px;
+    }
+</style>
